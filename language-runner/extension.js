773e5482fef2a5fe0ee2be059f039be6009f4c6e
Password-Classified <85027459+Password-Classified@users.noreply.github.com>
2021-11-06 18:58:52 +0000

    Add rust support

--- language-runner/extension.js
+++ language-runner/extension.js
@@ -23,34 +23,44 @@ function activate(context) {
 		let file_name = vscode.window.activeTextEditor.document.fileName;
 		vscode.workspace.saveAll(false)
 
-		if (file_name.slice(file_name.length - 3, file_name.length) == '.vb') {
+		if (file_name.slice(file_name.length - 3, file_name.length) == '.vb') { // VB.NET
 			RunnerTerminal.sendText('vbc "' + vscode.window.activeTextEditor.document.fileName + '" /out:"CompiledVB.exe"');
 			RunnerTerminal.sendText('CompiledVB.exe')
 
-		} else if (file_name.slice(file_name.length - 3, file_name.length) == '.py') {
-			RunnerTerminal.sendText('py "' + vscode.window.activeTextEditor.document.fileName + '"');
-		} else if (file_name.slice(file_name.length - 3, file_name.length) == '.js') {
-			RunnerTerminal.sendText('node "' + vscode.window.activeTextEditor.document.fileName + '"');
-			
-		} else if (file_name.slice(file_name.length - 2, file_name.length) == '.c') {
+		} else if (file_name.slice(file_name.length - 2, file_name.length) == '.c') { // C
 			RunnerTerminal.sendText('gcc "' + vscode.window.activeTextEditor.document.fileName + '" -o "CompiledC.exe"');
 			RunnerTerminal.sendText('CompiledC.exe')
-			
-		} else if (file_name.slice(file_name.length - 3, file_name.length) == '.hs') {
-			RunnerTerminal.sendText('ghc "' + vscode.window.activeTextEditor.document.fileName + '" -o "CompiledHS.exe"');
-			RunnerTerminal.sendText('CompiledHS.exe')
-
-		} else if (file_name.slice(file_name.length - 4, file_name.length) == '.cpp') {
+		} else if (file_name.slice(file_name.length - 4, file_name.length) == '.cpp') {  // C++
 			RunnerTerminal.sendText('g++ "' + vscode.window.activeTextEditor.document.fileName + '" -o "CompiledCPP.exe"');
 			RunnerTerminal.sendText('CompiledCPP.exe')
-		} else if (file_name.slice(file_name.length - 3, file_name.length) == '.cs') {
+		} else if (file_name.slice(file_name.length - 3, file_name.length) == '.cs') { // C#
 			RunnerTerminal.sendText('csc /t:exe /outCompiledCS.exe "' + vscode.window.activeTextEditor.document.fileName + '"');
 			RunnerTerminal.sendText('CompiledCS.exe')
 
-		} else if (file_name.slice(file_name.length - 3, file_name.length) == '.go') {
+		} else if (file_name.slice(file_name.length - 3, file_name.length) == '.go') { // Go
 			RunnerTerminal.sendText('go run "' + vscode.window.activeTextEditor.document.fileName + '"');
-		} else if (file_name.slice(file_name.length - 2, file_name.length) == '.r') {
+
+		} else if (file_name.slice(file_name.length - 3, file_name.length) == '.py') { // Python
+			RunnerTerminal.sendText('py "' + vscode.window.activeTextEditor.document.fileName + '"');
+
+		} else if (file_name.slice(file_name.length - 3, file_name.length) == '.js') { // Node JS
+			RunnerTerminal.sendText('node "' + vscode.window.activeTextEditor.document.fileName + '"');
+
+
+		} else if (file_name.slice(file_name.length - 2, file_name.length) == '.rs') { // Rust
+			RunnerTerminal.sendText('rustc  -o "CompiledRust.exe" "' + vscode.window.activeTextEditor.document.fileName + '"');
+			RunnerTerminal.sendText('CompiledRust.exe')
+
+		} else if (file_name.slice(file_name.length - 2, file_name.length) == '.exe') { // Machine Code
+			RunnerTerminal.sendText('"' + vscode.window.activeTextEditor.document.fileName + '"');
+
+		} else if (file_name.slice(file_name.length - 3, file_name.length) == '.hs') { // Haskell
+			RunnerTerminal.sendText('ghc "' + vscode.window.activeTextEditor.document.fileName + '" -o "CompiledHS.exe"');
+			RunnerTerminal.sendText('CompiledHS.exe')
+
+		} else if (file_name.slice(file_name.length - 2, file_name.length) == '.r') { // R
 			RunnerTerminal.sendText('rscript "' + vscode.window.activeTextEditor.document.fileName + '"');
+
 		} else {
 			vscode.window.showErrorMessage('Language Runner cannot run a ' + String(file_name.slice(file_name.length - 2, file_name.length)) + ' file.');
 		};
